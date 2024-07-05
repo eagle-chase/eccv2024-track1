c52f11c95369ff6fab49e6702c3b868afa289a22
eagle-chase <zxc876163546@gmail.com>
2024-07-05 02:44:42 +0000

    chat transformer

--- chat.py
+++ chat.py
@@ -4,19 +4,21 @@ from PIL import Image
 import torch
 from transformers import AutoProcessor, LlavaForConditionalGeneration
 
-model_id = "xtuner/llava-llama-3-8b-v1_1-transformers"
+# model_id = "xtuner/llava-llama-3-8b-v1_1-transformers"
 
 prompt = ("<|start_header_id|>user<|end_header_id|>\n\n<image>\nWhat are these?<|eot_id|>"
           "<|start_header_id|>assistant<|end_header_id|>\n\n")
 image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
 
+# 
+model_path = 'model/llava-llama-3-8b-v1_1-transformers'
 model = LlavaForConditionalGeneration.from_pretrained(
-    model_id, 
+    model_path, 
     torch_dtype=torch.float16, 
     low_cpu_mem_usage=True, 
 ).to(0)
 
-processor = AutoProcessor.from_pretrained(model_id)
+processor = AutoProcessor.from_pretrained(model_path)
 
 
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
@@ -24,4 +26,3 @@ inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
 
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
-
