ba1df27eadce6cd82b71d7f58398c987de013a1d
eagle-chase <zxc876163546@gmail.com>
2024-07-07 09:37:38 +0000

    ch

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,5 +1,5 @@
 python infer/infer.py \
-    --input-path eval/data/coda-lm/CODA-LM-chinese/Val10/vqa_anno/driving_suggestion.jsonl \
+    --input-path eval/data/coda-lm/CODA-LM/Val10/vqa_anno/driving_suggestion.jsonl \
     --image-root eval/data/coda-lm/ \
     --model-path model/llava-llama-3-8b-v1_1-transformers \
     --save-path eval/result/pred \
@@ -7,7 +7,7 @@ python infer/infer.py \
     --device-id 0 
 
 python infer/infer.py \
-    --input-path eval/data/coda-lm/CODA-LM-chinese/Val10/vqa_anno/general_perception.jsonl \
+    --input-path eval/data/coda-lm/CODA-LM/Val10/vqa_anno/general_perception.jsonl \
     --image-root eval/data/coda-lm/ \
     --model-path model/llava-llama-3-8b-v1_1-transformers \
     --save-path eval/result/pred \
@@ -15,7 +15,7 @@ python infer/infer.py \
     --device-id 0 
 
 python infer/infer.py \
-    --input-path eval/data/coda-lm/CODA-LM-chinese/Val10/vqa_anno/region_perception.jsonl \
+    --input-path eval/data/coda-lm/CODA-LM/Val10/vqa_anno/region_perception.jsonl \
     --image-root eval/data/coda-lm/ \
     --model-path model/llava-llama-3-8b-v1_1-transformers \
     --save-path eval/result/pred \
