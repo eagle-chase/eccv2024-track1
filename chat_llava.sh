18d3034336f0085e60382cf666d30a0c23c4bf70
ywonchall <ywonchall@163.com>
2024-07-08 14:21:47 +0800

    rm chat script

--- chat_llava.sh
+++
@@ -1,5 +0,0 @@
-xtuner chat model/Meta-Llama-3-8B-Instruct \
-  --visual-encoder model/clip-vit-large-patch14-336 \
-  --llava model/llava-llama-3-8b-finetune \
-  --prompt-template llama3_chat \
-  --image data/coda-lm/val/images_w_boxes/0001_object_2.jpg
