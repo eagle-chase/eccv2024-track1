0085172182920560bb8d4ec5e75fafe5cf6470ef
ywonchall <ywonchall@163.com>
2024-07-10 18:31:02 +0800

    merge

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,5 +1,41 @@
 ## set your project path
-export PYTHONPATH="${PYTHONPATH}:/data/home/ouyangmt/eccv2024-track1"
+export PYTHONPATH="${PYTHONPATH}:${PWD}"
+LLM_PATH='model/Meta-Llama-3-8B-Instruct'
+VISUAL_ENCODER_PATH='model/clip-vit-large-patch14-336'
+LLAVA_PATH='model/llava_llama3_8b_instruct_qlora_clip_vit_large_p14_336_e4_gpu8_finetune'
+
+# ======================= xtuner infer =======================
+
+python infer/inference.py \
+    --llava_model_path $LLAVA_PATH \
+    --llm_model_path $LLM_PATH \
+    --input-path data/coda-lm/CODA-LM/Val/vqa_anno/driving_suggestion.jsonl \
+    --image-root data/coda-lm/ \
+    --save-path eval/result/pred \
+    --task-name driving_suggestion \
+    --model-type xtuner \
+    # --visual_encoder_path $VISUAL_ENCODER_PATH \
+
+# python infer/inference.py \
+#     --llava_model_path $LLAVA_PATH \
+#     --llm_model_path $LLM_PATH \
+#     --visual_encoder_path $VISUAL_ENCODER_PATH \
+#     --input-path data/coda-lm/CODA-LM/Val/vqa_anno/general_perception.jsonl \
+#     --image-root data/coda-lm/ \
+#     --save-path eval/result/pred \
+#     --task-name general_perception \
+#     --model-type xtuner \
+
+# python infer/inference.py \
+#     --llava_model_path $LLAVA_PATH \
+#     --llm_model_path $LLM_PATH \
+#     --visual_encoder_path $VISUAL_ENCODER_PATH \
+#     --input-path data/coda-lm/CODA-LM/Val/vqa_anno/region_perception.jsonl \
+#     --image-root data/coda-lm/ \
+#     --save-path eval/result/pred \
+#     --task-name region_perception \
+#     --model-type xtuner \
+
 
 # ======================= huggingface infer =======================
 
@@ -30,35 +66,3 @@ export PYTHONPATH="${PYTHONPATH}:/data/home/ouyangmt/eccv2024-track1"
 #     --device-id 0 \
 #     --model-type huggingface \
 
-
-# ======================= xtuner infer =======================
-
-python infer/inference.py \
-    --llava_model_path model/llava-llama-3-8b-finetune \
-    --llm_model_path model/Meta-Llama-3-8B-Instruct \
-    --visual_encoder_path model/clip-vit-large-patch14-336 \
-    --input-path data/coda-lm/CODA-LM/Val/vqa_anno/driving_suggestion.jsonl \
-    --image-root data/coda-lm/ \
-    --save-path eval/result/pred \
-    --task-name driving_suggestion \
-    --model-type xtuner \
-
-# python infer/inference.py \
-#     --llava_model_path model/llava-llama-3-8b-finetune \
-#     --llm_model_path model/Meta-Llama-3-8B-Instruct \
-#     --visual_encoder_path model/clip-vit-large-patch14-336 \
-#     --input-path data/coda-lm/CODA-LM/Val/vqa_anno/general_perception.jsonl \
-#     --image-root data/coda-lm/ \
-#     --save-path eval/result/pred \
-#     --task-name general_perception \
-#     --model-type xtuner \
-
-# python infer/inference.py \
-#     --llava_model_path model/llava-llama-3-8b-finetune \
-#     --llm_model_path model/Meta-Llama-3-8B-Instruct \
-#     --visual_encoder_path model/clip-vit-large-patch14-336 \
-#     --input-path data/coda-lm/CODA-LM/Val/vqa_anno/region_perception.jsonl \
-#     --image-root data/coda-lm/ \
-#     --save-path eval/result/pred \
-#     --task-name region_perception \
-#     --model-type xtuner \
